f67562e30b433046efd38e3d3bd483323c811d16
Bhavya Goyal <bhavyagoyal.workday@gmail.com>
2025-05-25 22:46:48 +0530

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,9 +1,9 @@
 using Documenter, DocumenterVitepress
 
 makedocs(; 
-    sitename = "Div-Brewery", 
+    sitename = "Bee-Brewery", 
     format=DocumenterVitepress.MarkdownVitepress(
-        repo = "github.com/divital-coder/Div-Brewery", 
+        repo = "github.com/BhavyaQuantDeveloper/Bee-Brewery", 
         devbranch = "main",
         devurl = "dev",
     ),
@@ -21,7 +21,7 @@ makedocs(;
 
 # This is the critical part that creates the version structure
 DocumenterVitepress.deploydocs(;
-    repo = "github.com/divital-coder/Div-Brewery", 
+    repo = "github.com/BhavyaQuantDeveloper/Bee-Brewery", 
     devbranch = "main",
     push_preview = true,
 )
